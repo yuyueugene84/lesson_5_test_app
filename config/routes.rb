68ee0f2b08a80929dcb2aebd56c8275e12d6c4bf
yuyueugene84 <yuyueugene84@gmail.com>
2015-11-29 06:52:17 +0800

    added users index page to test_app

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  # 增加一個叫 users 的 resource
+  resources :users
+  
   # Example resource route with options:
   #   resources :products do
   #     member do
